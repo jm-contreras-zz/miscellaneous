33ce3cfe1ed2c0f39c33cfbdd35544022074f971
Juan Manuel Contreras <juan.manuel.contreras.87@gmail.com>
2014-05-17 10:53:35 -0400

    Update monty_hall.py

--- monty_hall.py
+++ monty_hall.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Feb  1 19:45:17 2014
+Monty Hall problem (http://en.wikipedia.org/wiki/Monty_Hall_problem) simulator.
+Inspired by Harvard University's CS109 Data Science (http://bit.ly/1agO1GB).
 @author: Juan Manuel Contreras (juan.manuel.contreras.87@gmailcom)
 
-Monty Hall problem (http://en.wikipedia.org/wiki/Monty_Hall_problem) simulator.
-Inspired by Harvard Universiy's CS109 Data Science (http://bit.ly/1agO1GB).
-
-Once imported, monty_hall's method 'simulate' is called with three variables.
-    n_doors = number of doors in the game
-    switch  = guess strategy after goat reveal (0 = don't switch, 1 = switch)
-    n_sim   = number of simulations to run
+The user must specify three variables.
+n_doors  = the number of doors in the game
+n_open   = the number of doors opened by the host
+switch   = should the door be switched after the goat reveal? 0 = no, 1 = yes
+n_sim    = the number of simulations to run 
 """
 
-def simulate(n_doors, switch, n_sim):
+def simulator(n_doors, n_open, switch, n_sim):
 
     # Import modules
-    from numpy import array, mean, random
+    from numpy import mean, random
 
     # Create array of doors
     doors = range(n_doors)
@@ -31,25 +31,21 @@ def simulate(n_doors, switch, n_sim):
     for i in range(n_sim):
         # Determine which doors can host the goats (no prize, not guessed)
         can_host_goat = list(set(doors) - set([prize_doors[i], guesses[i]]))
-        # If only one door, choose it
-        if len(can_host_goat) == 1:
-            goat_doors[i] = can_host_goat[0]
-        # Otherwise, choose between the two options
-        else:
-            goat_doors[i] = random.choice(can_host_goat, 1)[0]
-            goat_doors = array(goat_doors)
-    
-    # Switch guesses, if it is part of the strategy
+        # If the guessed door has the prize, choose which goat door to open
+        goat_doors[i] = random.choice(can_host_goat, n_open, replace=False)
+    # Switch guesses, if part of strategy
     if switch:
         for i in range(n_sim):
-            guesses[i] = list(set(doors) - set([guesses[i], goat_doors[i]]))[0]
-        with_without = 'With'
+            this_goat_list = list(goat_doors[i])
+            this_goat_list.append(guesses[i])
+            possible_guesses = list(set(doors) - set(this_goat_list))
+            guesses[i] = random.choice(possible_guesses, 1)[0]
+        word_report = 'With'
     else:
-        with_without = 'Without'
-
-    # Calculate the percentage of correct guesses
-    percentage = int(round(100 * mean(prize_doors == guesses)))    
+        word_report = 'Without'
+    # Calculate percentage of correct guesses
+    percentage = mean(prize_doors == guesses) * 100
     
     # Report results
-    print with_without + ' a switching strtegy, a player wins ' + \
-          str(percentage) + '% of simulations.'
+    print '%s a switching strategy, you win %s of simulations.' % \
+    (word_report, int(percentage))
