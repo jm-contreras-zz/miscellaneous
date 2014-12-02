afad78ff6f3d4c4e35ae9afbb88829a5516ca828
Juan Manuel Contreras <juan.manuel.contreras.87@gmail.com>
2014-12-02 15:27:04 -0500

    Create text_miner.py

---
+++ text_miner.py
@@ -0,0 +1,18 @@
+# Import modules
+import numpy as np
+import pandas as pd
+
+# Define text miner function
+def text_miner(text, miner):
+    df = pd.DataFrame(np.zeros((len(text), len(miner))), columns=miner.keys())
+    for key in miner:
+        df[key] = text.str.contains('|'.join(miner[key]), case=False)
+    return df.astype('int32')
+
+# Create text series and miner dictionary
+text = pd.Series(['I ran to Iran', 'You attack Iraq', 'England is bland', 'We dance in France', 'They wait in Kuwait'])
+miner = {'middle_east': ['Iran', 'Iraq', 'Kuwait'],
+         'verb': ['ran', 'attack', 'dance', 'wait']}
+
+# Apply text miner
+mined_text_df = text_miner(text, miner)
