2d143c2309c99785d93a919be498fb0639acc29d
Juan Manuel Contreras <juan.manuel.contreras.87@gmail.com>
2015-03-20 23:22:47 -0400

    Update and rename ltr_clock.py to LDRclock.py

--- LDRclock.py
+++ LDRclock.py
@@ -1,4 +1,4 @@
-# Import modules
+# Import module
 from datetime import datetime as dt
 
 # Declare now and then
@@ -8,19 +8,19 @@ then = dt.strptime('Apr 2 2015 10:59PM', '%b %d %Y %I:%M%p')
 # Compute delta
 delta = then - now
 
-# Determine values
+# Determine time values
 v = [delta.days, delta.seconds // 3600, delta.seconds // 60 % 60, delta.seconds % 60]
 
-# Determine units
+# Determine time units
 u = ['days', 'hours', 'minutes', 'seconds']
 
-# Drop s from units, if necessary
+# Drop 's' from time units, if necessary
 for i, this_u in enumerate(u):
     if v[i] == 1:
         u[i] = this_u[:-1]
 
 # Write report
-report = 'You will see Eva in {} {}, {} {}, {} {}, and {} {}.'
+report = 'You will see your sweetheart in {} {}, {} {}, {} {}, and {} {}.'
 
 # Print it
 print report.format(v[0], u[0], v[1], u[1], v[2], u[2], v[3], u[3])
